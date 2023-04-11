8c8a3d75f3e9f3673ef5c234b8c16358b2248e43
Sergey Glebov <sergeykglebov@gmail.com>
2023-04-11 14:19:34 +0200

    Minor cleanup

--- tiny_csv/include/tiny_csv/type_loader.hpp
+++ tiny_csv/include/tiny_csv/type_loader.hpp
@@ -18,14 +18,14 @@ namespace tiny_csv {
 
 template<typename T, typename Enable = void>
 struct Loader {
-    static T Load(T& t, const char *buf, size_t len, const ParserConfig &cfg) {
+    static T Load(T& t, const char *buf, size_t len, const ParserConfig &) {
         static_assert(std::is_void_v<T>, "Unsupported type");
     }
 };
 
 template<typename T>
 struct Loader<T, std::enable_if_t<std::is_integral_v<T>>> {
-    static T Load(const char *buf, size_t len, const ParserConfig &cfg) {
+    static T Load(const char *buf, size_t len, const ParserConfig &) {
         T value;
         std::from_chars(buf, buf + len, value);
         return value;
@@ -34,7 +34,7 @@ struct Loader<T, std::enable_if_t<std::is_integral_v<T>>> {
 
 template<typename T>
 struct Loader<T, std::enable_if_t<std::is_floating_point_v<T>>> {
-    static T Load(const char *buf, size_t len, const ParserConfig &cfg) {
+    static T Load(const char *buf, size_t len, const ParserConfig &) {
         T value;
         std::from_chars(buf, buf + len, value);
         return value;
@@ -43,7 +43,7 @@ struct Loader<T, std::enable_if_t<std::is_floating_point_v<T>>> {
 
 template<typename T>
 struct Loader<T, std::enable_if_t<std::is_same_v<T, std::string>>> {
-    static T Load(const char *buf, size_t len, const ParserConfig &cfg) {
+    static T Load(const char *buf, size_t len, const ParserConfig &) {
         return std::string(buf, len);
     }
 };
